6ff82ab46d6460759d21a88902908d1d4079c2cc
256217 <80380160+vivek28121997@users.noreply.github.com>
2021-04-13 14:54:20 +0530

    Update w.c

--- 3_Implementation/src/w.c
+++ 3_Implementation/src/w.c
@@ -1,4 +1,25 @@
-#include "wmcs.h"
+# include "wmcs.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdio_ext.h>
+# include <time.h>
+# include <conio.h>
+# include <unistd.h>
+# include <dos.h>
+
+void stateOnOff();
+void mainmenu();
+void Washclothes();
+void Dryclothes();
+void WashMenu();
+void soak();
+void getTimesoak();
+void Temperature();
+void viewlog();
+void processlog();
+char result[4];
+int main(void);
+int times,count;
 
 int _tmain(int argc, char* argv[])
  {
