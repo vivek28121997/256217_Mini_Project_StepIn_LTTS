dd43334c8707c987ca0da92bbff26d9e42376559
256217 <80380160+vivek28121997@users.noreply.github.com>
2021-04-14 18:13:31 +0530

    Update test_wmcs.c

--- 3_Implementation/test/test_wmcs.c
+++ 3_Implementation/test/test_wmcs.c
@@ -3,11 +3,12 @@
 
 /* Modify these two lines according to the project */
 #include "wmcs.h"
+#ifndef PROJECT_NAME
 #define PROJECT_NAME    "Washing_Machine"
 
 /* Prototypes for all the test functions */
 void test_mainmenu(void);
-void test_stateOnOff(void);
+void test_start(void);
 void test_Washclothes(void);
 void test_WashMenu(void);
 void test_Soak(void);
@@ -30,7 +31,7 @@ int main()
 
 /* Run Test functions */
   //RUN_TEST(test_mainmenu);
-  RUN_TEST(test_stateOnOff);
+  RUN_TEST(test_start);
   //RUN_TEST(test_Washclothes);
   //RUN_TEST(test_WashMenu);
   //RUN_TEST(test_Soak);
@@ -44,7 +45,8 @@ int main()
 }
 
 /* Write all the test functions */ 
-void test_stateOnOff(void) {
-  TEST_ASSERT_EQUAL(mainmenu(), stateOnOff("s"));
+void test_start(void) {
+  //UNITY_TEST_ASSERT("Washing machine is ON.",start("s"));
   //TEST_ASSERT_EQUAL(Dryclothes(), mainmenu("d"));
 }
+#endif 
