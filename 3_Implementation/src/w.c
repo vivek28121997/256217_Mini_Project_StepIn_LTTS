a7652338fcf1fc41960ffd1c6555e645af5c1dfb
256217 <80380160+vivek28121997@users.noreply.github.com>
2021-04-15 23:16:16 +0530

    Update w.c

--- 3_Implementation/src/w.c
+++ 3_Implementation/src/w.c
@@ -1,16 +1,5 @@
 # include "wmcs.h"
 
-int _tmain(int argc, char* argv[])
- {
-  start();
-  return 0;
- }
- int main(void) {
- time_t timer = time(NULL);
- printf("current time is %s", ctime(&timer));
- start();
-}
-
 void start() {
  char ch;
  printf("\n**************************************************************");
