7cbb860c75d648130ea602270c7fa5ad1d2f1fa2
256217 <80380160+vivek28121997@users.noreply.github.com>
2021-04-15 22:14:17 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,4 +1,4 @@
-#include <wmcs.h>
+#include "wmcs.h"
 
 /* Start of the application */
 int _tmain(int argc, char* argv[])
@@ -6,8 +6,11 @@ int _tmain(int argc, char* argv[])
   start();
   return 0;
  }
+
  int main(void) {
  time_t timer = time(NULL);
  printf("current time is %s", ctime(&timer));
  start();
+  
+  return 0;
 }
