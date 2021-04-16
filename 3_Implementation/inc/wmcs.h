afffe0acfa5ec85a3d5e461e254bcd2c103d2d44
256217 <80380160+vivek28121997@users.noreply.github.com>
2021-04-16 15:55:08 +0530

    Update wmcs.h

--- 3_Implementation/inc/wmcs.h
+++ 3_Implementation/inc/wmcs.h
@@ -1,19 +1,30 @@
+/**
+ * @file wmcs.h
+ * @author Vivek Kumar Yadav SFID:256217
+ * @brief contains all the headers required.
+ * @version 0.1
+ * @date 2021-04-09
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef __WMCS_H__
 #define __WMCS_H__
 
 # include <stdio.h>
 # include <stdlib.h>
-//# include <stdio_ext.h>
+# include <stdbool.h>
+//# include <stdio_ext.h> //not supported by the vcode command line
 # include <time.h>
-//# include <conio.h>
+//# include <conio.h> //not supported by the linux command line
 # include <unistd.h>
-//# include <dos.h>
+//# include <dos.h> //not supported by the linux command line
 
 int main(void);
 void start();
 void mainmenu();
 void Loadsize();
-void System_checks();
+int System_checks();
 void WashMenu();
 void soak();
 void soakTime();
